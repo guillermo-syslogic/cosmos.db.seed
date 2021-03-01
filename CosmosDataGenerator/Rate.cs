7f276604b3faa6c7505a15e294cc7e4a56c775e8
Brian Pinkowski <bpinkowski@syslogicinc.com>
2021-03-01 08:46:30 -0600

    Data seeder updates

---
+++ CosmosDataGenerator/Rate.cs
@@ -0,0 +1,26 @@
+﻿namespace CosmosDataGenerator
+{
+    public class Rate
+    {
+        public RateType RateType { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public enum RateType
+    {
+        Flat = 0,
+        Hourly = 1,
+        HalfDay = 2,
+        Daily = 3,
+        Weekly = 4,
+        Monthly = 5,
+        Extended = 6,
+        OneDay = 7,
+        TwoDay = 8,
+        ThreeDay = 9,
+        FourDay = 10,
+        FiveDay = 11,
+        AdditionalDay = 12,
+        Weekend = 13
+    }
+}
---
+++ CosmosDataGenerator/RateProgram.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CosmosDataGenerator
+{
+    public class RateProgram
+    {
+        [JsonProperty("id")]
+        public string RateProgramId { get; set; }
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; }
+        public List<Rate> Rates { get; set; }
+    }
+}
